89550f7f164074a71ab5738bdc784789c25ef4a6
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-01-31 14:37:32 -0800

    Add message interface

--- src/integrator.py
+++ src/integrator.py
@@ -1,1 +1,23 @@
 __author__ = 'dibyo'
+
+from datetime import datetime as dt, timezone
+
+
+class Message(object):
+    """
+    Data arrives as a set of messages.  This defines the message
+    interface.
+    """
+    def __init__(self, message):
+        self.message = message
+        self.timestamp = dt.now(tz=timezone.utc)
+
+    def __eq__(self, other: Message):
+        """
+        Two messages are equal if they have the same message
+        :param other: the other message to be considered
+        :return:
+        """
+        return self.message == other.message
+
+
